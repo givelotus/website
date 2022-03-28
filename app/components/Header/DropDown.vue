73103797327ea78938ffc82cddaa38d419047ea8
Vic Cieslak <wiktorcie@gmail.com>
2022-03-28 13:54:01 +0200

    Add working dropdown menu

---
+++ app/components/Header/DropDown.vue
@@ -0,0 +1,105 @@
+<template>
+  <div id="app">
+    <ul class="header__navbar">
+      <li class="header__item">
+        <a href="#" class="header__link">
+          <transition name="slide-fade">
+            <v-btn
+              key="off"
+              @click="show = !show"
+              text
+              class="menu-link"
+              >More</v-btn>
+          </transition>
+        </a>
+
+        <transition name="dropdown">
+          <div class="header__dropdown-menu" :class="{ active: show }" v-if="show">
+            <ul class="header__dropdown-menu-nav">
+              <li class="header__dropdown-menu-item">
+                <a href="https://explorer.givelotus.org/" target="_blank" class="header__dropdown-menu-link" title="Block Explorer">
+                  <div class="header__dropdown-menu-svg">
+                    <v-icon color="grey darken-2"> grid_view</v-icon>
+                  </div>
+                  <div class="header__dropdown-menu-text">
+                    {{ $t('lotusLanding.header_block_explorer') }}
+                  </div>
+                </a>
+              </li>
+              <li class="header__dropdown-menu-item">
+                <a href="https://docs.givelotus.org/" target="_blank" class="header__dropdown-menu-link" title="Personal info">
+                  <div class="header__dropdown-menu-svg">
+                    <v-icon color="grey darken-2">article</v-icon>
+                  </div>
+                  <div class="header__dropdown-menu-text">
+                    {{ $t('lotusLanding.documentation') }}
+                  </div>
+                </a>
+              </li>
+              <li class="header__dropdown-menu-item">
+                <a :href="getLocalizedHref('/credits')" class="header__dropdown-menu-link" title="Personal info">
+                  <div class="header__dropdown-menu-svg">
+                    <v-icon color="grey darken-2">groups</v-icon>
+                  </div>
+                  <div class="header__dropdown-menu-text">
+                    Contributors
+                  </div>
+                </a>
+              </li>
+              <li class="header__dropdown-menu-item">
+                <a :href="getLocalizedHref('/calculator')" class="header__dropdown-menu-link" title="Personal info">
+                  <div class="header__dropdown-menu-svg">
+                    <v-icon color="grey darken-2">calculate</v-icon>
+                  </div>
+                  <div class="header__dropdown-menu-text">
+                    Rewards calculator
+                  </div>
+                </a>
+              </li>
+
+            </ul>
+          </div>
+        </transition>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import './dropdown.scss';
+@import './header-style.scss';
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      show: true,
+    }
+  },
+  methods: {
+    getLocalizedHref(url) {
+      // refactor using composition API
+      let href = ''
+      // gb is a default language so /gb/#faq doesnt exist and 404s
+      // but we need to include prefix to localized page so there is
+      // no unnecessary redirections when /de/ user hits other pages
+      // e.g /calculator nuxt u18n redirects to /de/calculator
+      // it leads to poor UX
+      if (this.$i18n.locale != 'gb') {
+        href = '/' + this.$i18n.locale + url
+      } else {
+        href = '/' + url
+      }
+      return href
+    },
+  },
+  created() {
+    window.addEventListener('click', e => {
+      if (!this.$el.contains(e.target)) {
+        this.show = false
+      }
+    })
+  },
+}
+</script>
