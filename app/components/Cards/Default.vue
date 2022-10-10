d716e3cff4d2bcfab5c3187bfac2e7f9209e2ff7
Shammah Chancellor <shammah.chancellor@gmail.com>
2022-10-10 09:49:56 -0700

    Simplify project card styling by fully using Vuetify (#79)
    Remove most of the custom styling requirements, and just use Vuetify
    to manage the card styles.

--- app/components/Cards/Default.vue
+++ app/components/Cards/Default.vue
@@ -1,20 +1,37 @@
 <template>
-  <v-card class="default-card">
-    <figure class="image-container">
-      <img :src="img" alt="img" />
-    </figure>
-    <div class="text pt-10">
-      <h6 class="title">{{ title }}</h6>
+  <v-card class="d-flex flex-column" height="450px">
+    <v-img :src="img" alt="img" contain height="170px" />
+    <v-card-title class="mb-none">
+      {{ title }}
+    </v-card-title>
+    <v-list-item class="align-start">
       {{ desc }}
-    </div>
-    <v-btn color="secondary" class="button" :href="url" target="_blank">
-      {{ button }}
-    </v-btn>
+    </v-list-item>
+    <v-card-actions class="mt-auto text-top">
+      <v-btn color="secondary" class="button" :href="url" target="_blank">
+        {{ button }}
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
-<style lang="scss" scoped>
-@import './card-style.scss';
+<style scoped lang="scss">
+.button {
+  width: 50%;
+  @include margin-left(50%);
+  height: 43px;
+  line-height: 44px;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  @include shadow10;
+  [dir='ltr'] & {
+    border-radius: 20px 0 0 0;
+  }
+  [dir='rtl'] & {
+    border-radius: 0 20px 0 0;
+  }
+}
 </style>
 
 <script>
